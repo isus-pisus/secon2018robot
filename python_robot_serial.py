c3497359d0b3d83d73b9ecefa082f05c2f2cfb30
1-isus <think.believe.cr8@gmail.com>
2018-01-23 19:25:53 -0500

    Sending command to arduino through serial (untested)

---
+++ python_robot_serial.py
@@ -0,0 +1,22 @@
+#!/usr/bin/env python
+import serial
+from serial import SerialException
+import time
+ser = serial.Serial()
+ser.port = "/dev/ttyACM1"
+ser.baudrate = 9600 #Define bause rate speed, set same on arduino
+try:
+    ser.open()
+    ser.write("Move Forward".encode())
+    ser.write("Move Reverse".encode())
+    ser.close()
+except SerialException as e:
+    print("error opening serial port " + str(e))
+
+finally:
+    if ser.isOpen():
+        ser.flushInput() #flush input buffer, discarding all its contents
+        ser.write("Move Forward".encode())
+        ser.write("Move Reverse".encode())
+        ser.flushOutput()#flush output buffer, aborting current output
+        ser.close()
