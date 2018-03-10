fca1796b621311570650ae0833df8a5bdba46b9f
isus-pisus <romchris01@gmail.com>
2018-03-10 02:52:58 -0500

    added destination_C array and changed baudrate to 57600

--- communicate_v2.py
+++ communicate_v2.py
@@ -1,62 +1,131 @@
-import time, os
 import numpy as np
 import serial
 
 ser = serial.Serial()
-ser.baudrate = 9600
-ser.timeout = 10
+ser.baudrate = 57600
+ser.timeout = None
 ser.port = '/dev/ttyACM0'
+
+""" Destination array is in the format
+    destination_array = np.array([
+        [
+            all commands for the left side of the field ie. a '0'
+        ],
+        [
+            all commands for the right side of the field ie. a '1'
+        ]
+    ])
+"""
 
 destination_A = np.array([
     [
-        ['FORWARD', 1000],
-        ['RIGHT', 700],
+        ['LEFT', 700],
         ['STOP', 0],
-        ['LEFT', 500],
+        ['FORWARD', 1000],
+        ['STOP', 0],
+        # ['DOSTUFF', 0],
+        ['BACK', 1000],
+        ['STOP', 0],
+        ['RIGHT', 700],
         ['STOP', 0]
     ],
     [
-        ['FORWARD', 5000],
-        ['LEFT', 700],
+        ['RIGHT', 700],
         ['STOP', 0],
-        ['RIGHT', 500],
+        ['FORWARD', 1000],
+        ['STOP', 0],
+        # ['DOSTUFF', 0],
+        ['BACK', 1000],
+        ['STOP', 0],
+        ['LEFT', 700],
         ['STOP', 0]
-
     ]
 ])
 
 destination_B = np.array([
     [
         ['FORWARD', 5000],
-        ['RIGHT', 700],
         ['STOP', 0],
-        ['LEFT', 500],
-        ['STOP', 0]
-    ],
-    [
-        ['FORWARD', 5000],
         ['LEFT', 700],
+        ['STOP', 0],
+        ['FORWARD', 750],
+        ['STOP', 0],
+        ['BACK', 750],
         ['STOP', 0],
         ['RIGHT', 500],
         ['STOP', 0]
 
+    ],
+    [
+        ['FORWARD', 5000],
+        ['STOP', 0],
+        ['RIGHT', 700],
+        ['STOP', 0],
+        ['FORWARD', 750],
+        ['STOP', 0],
+        ['BACK', 750],
+        ['STOP', 0],
+        ['LEFT', 500],
+        ['STOP', 0]
     ]
 ])
+
+destination_C = np.array([
+    [
+        ['FORWARD', 1000],
+        ['STOP', 0],
+        # ['PICKUP', 700],
+        ['FORWARD', 2000],
+        ['STOP', 0],
+        # ['TURNFLAG', 0],
+        ['BACK', 2000],
+        ['LEFT', 0],
+        ['LEFT', 0],
+        ['STOP', 0],
+        ['FORWARD', 10000],
+        ['STOP', 0],
+        ['RIGHT', 0],
+        ['STOP', 0],
+        ['FORWARD', 700],
+
+    ],
+    [
+        ['FORWARD', 1000],
+        ['STOP', 0],
+        # ['PICKUP', 700],
+        ['FORWARD', 2000],
+        ['STOP', 0],
+        # ['TURNFLAG', 0],
+        ['BACK', 2000],
+        ['LEFT', 0],
+        ['LEFT', 0],
+        ['STOP', 0],
+        ['FORWARD', 10000],
+        ['STOP', 0],
+        ['LEFT', 0],
+        ['STOP', 0],
+        ['FORWARD', 700],
+    ]
+])
+
 
 def number_of_steps(distance):
     return distance / ONE_REVOLUTION * STEPS_PER_REVOLUTION
 
+
 def arduino_msg(x):
     ser.write((x[0]+","+str(x[1])).encode())
-    # ser.write((x[0], x[1]).encode())
+
 
 def get_direction():
     return [0, 1, 1]
 
 
 def join_arrays(x):
-    final_command = np.concatenate((destination_A[x[0]], destination_B[x[0]]), out=None)
+    final_command = np.concatenate(
+        (destination_A[x[0]], destination_B[x[1]], destination_C[x[2]]), out=None)
     return final_command
+
 
 cmd = iter(join_arrays(get_direction()))
 
@@ -65,13 +134,11 @@ if __name__ == '__main__':
     command = next(cmd)
     print(command)
     arduino_msg(command)
-
-    # print('sent')
     while 1:
         ser.flushInput()
         is_finished = ser.read().decode('utf-8')
-        print('getting ', is_finished)
         if is_finished == '1':
+            print('Done!')
             command = next(cmd)
             print(command)
             arduino_msg(command)
