0d3fb3a2309f80f1f1d781c15d262d18673a4d88
isus-pisus <romchris01@gmail.com>
2018-04-13 14:15:28 -0500

    added  ir reader

--- communicate_v2.py
+++ communicate_v2.py
@@ -1,8 +1,10 @@
 import numpy as np
-import serial
+import serial, time
+from lcd import lcd
+from ir_decoder import get_ir_code
 
 ser = serial.Serial()
-ser.baudrate = 57600
+ser.baudrate = 9600
 ser.timeout = None
 ser.port = '/dev/ttyACM0'
 
@@ -19,7 +21,7 @@ ser.port = '/dev/ttyACM0'
 
 destination_A = np.array([
     [
-        ['LEFT', 700],
+        ['LEFT', 70],
         ['STOP', 0],
         ['FORWARD', 1000],
         ['STOP', 0],
@@ -44,7 +46,7 @@ destination_A = np.array([
 
 destination_B = np.array([
     [
-        ['FORWARD', 5000],
+        ['FORWARD', 50],
         ['STOP', 0],
         ['LEFT', 700],
         ['STOP', 0],
@@ -57,7 +59,7 @@ destination_B = np.array([
 
     ],
     [
-        ['FORWARD', 5000],
+        ['FORWARD', 500],
         ['STOP', 0],
         ['RIGHT', 700],
         ['STOP', 0],
@@ -72,7 +74,7 @@ destination_B = np.array([
 
 destination_C = np.array([
     [
-        ['FORWARD', 1000],
+        ['FORWARD', 10],
         ['STOP', 0],
         # ['PICKUP', 700],
         ['FORWARD', 2000],
@@ -91,7 +93,7 @@ destination_C = np.array([
 
     ],
     [
-        ['FORWARD', 1000],
+        ['FORWARD', 100],
         ['STOP', 0],
         # ['PICKUP', 700],
         ['FORWARD', 2000],
@@ -111,10 +113,6 @@ destination_C = np.array([
 ])
 
 
-def number_of_steps(distance):
-    return distance / ONE_REVOLUTION * STEPS_PER_REVOLUTION
-
-
 def arduino_msg(x):
     ser.write((x[0]+","+str(x[1])).encode())
 
@@ -125,18 +123,29 @@ def get_direction():
 
 def join_arrays(x):
     final_command = np.concatenate(
-        (destination_A[x[0]], destination_B[x[1]], destination_C[x[2]]), out=None)
+        (destination_A[x[0]], destination_B[x[1]], destination_C[x[2]]))
     return final_command
 
 
-final_array_with_commands = join_arrays(get_direction())
-cmd = iter(final_array_with_commands)
+def check_for_valid_ir_signal():
+    while get_ir_code() == [1, 1, 1, 1, 1, 1, 1, 1]:
+        get_ir_code()
 
 if __name__ == '__main__':
+    check_for_valid_ir_signal()
+    final_destination = get_ir_code()
+
+    lcd.write_string(u'Destination')
+    lcd.cursor_pos = (1, 0)
+    lcd.write_string("".join(str(e) for e in final_destination))
+
+    final_array_with_commands = join_arrays(final_destination[5:])
+    cmd = iter(final_array_with_commands)
+
     ser.open()
     command = next(cmd)
-    print(command)
     arduino_msg(command)
+
     while 1:
         ser.flushInput()
         is_finished = ser.read().decode('utf-8')
